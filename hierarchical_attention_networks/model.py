5090e1fba2e34e62433017c063e38de42ec04265
binhnq <quangbinh.nguyentrong@gmail.com>
2019-05-15 09:07:03 +0700

    add multi attention

--- hierarchical_attention_networks/model.py
+++ hierarchical_attention_networks/model.py
@@ -152,3 +152,139 @@ class HierarchicalAttention(nn.Module):
 
         final_outputs = self.final_linear(sents_att)
         return final_outputs
+
+
+class MultiAttentionLayer(nn.Module):
+    def __init__(self, input_size, attention_size=64, num_att=5):
+        super(MultiAttentionLayer, self).__init__()
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.input_size = input_size
+        self.attention_size = attention_size
+        self.num_att = num_att
+        self.in_dense_layer = torch.nn.Linear(self.input_size, self.num_att*self.attention_size)
+
+        self.out_dense_layers = []
+        for i in range(self.num_att):
+            out_dense_layer = torch.nn.Linear(self.attention_size, 1, bias=False)
+            # out_dense_layer.cuda()
+            self.add_module(f"out_dense_{i}", out_dense_layer)
+            self.out_dense_layers.append(out_dense_layer)
+
+    def forward(self, seq_input, lengths):
+        """
+
+        :param seq_input: [batch_size, seq_len, emb]
+        :param lengths: [batch_size, 1]
+        :return:
+        """
+        # [batch_size, seq_len, emb_size] = seq_input.shape
+        out_dense = self.in_dense_layer(seq_input)  # [batch_size, seq_len, self.num_att * self.attention_size]
+        out_dense = torch.tanh(out_dense)
+
+        list_out_dense_0 = torch.split(out_dense, self.attention_size, dim=2)
+        # self.num_att * Tensor [batch_size, seq_len, self.attention_size]
+
+        list_out = []
+        list_out_weight = []
+        for index_att, out_dense in enumerate(list_out_dense_0):
+            out_dense_1 = self.out_dense_layers[index_att](out_dense)
+            exps = torch.exp(out_dense_1)
+            mask = create_mask(lengths, seq_input.shape[1]).to(self.device).unsqueeze(2)
+            masked_exps = exps * mask
+            sumed_exps = torch.sum(masked_exps, 1, keepdim=True)
+            weights_att = masked_exps/sumed_exps
+            output_of_attention = torch.sum(weights_att*seq_input, 1)
+
+            list_out.append(output_of_attention)
+            list_out_weight.append(weights_att.squeeze(2))
+
+        out = torch.cat(list_out, dim=-1)
+
+        return out, list_out_weight
+
+
+class MultiAttentionLayerV2(nn.Module):
+    def __init__(self, input_size, attention_size=64, num_att=5):
+        super(MultiAttentionLayerV2, self).__init__()
+        self.num_att = num_att
+        self.list_att = []
+        for i in range(self.num_att):
+            att_layer = AttentionLayer(input_size, attention_size)
+            att_layer.cuda()
+            self.list_att.append(att_layer)
+
+    def forward(self, seq_input, lengths):
+        """
+
+        :param seq_input: [batch_size, seq_len, emb]
+        :param lengths: [batch_size, 1]
+        :return:
+        """
+        list_att_output = []
+        list_weight = []
+
+        for i in range(self.num_att):
+            out, weight = self.list_att[i](seq_input, lengths)
+
+            list_att_output.append(out)
+            list_weight.append(weight)
+
+        out = torch.cat(list_att_output, dim=-1)
+        # out_weight = torch.cat()
+        return out, list_weight
+
+
+class HierarchicalMultiAttention(HierarchicalAttention):
+
+    def __init__(self, output_size, embedding_size, embedding_weight, num_att=5, lstm_hidden_size=256,
+                 lstm_num_layers=1, attention_size=64):
+        super(HierarchicalAttention, self).__init__()
+
+        self.num_att = num_att
+        self.word_embeddings = nn.Embedding.from_pretrained(embedding_weight)
+        self.lstm_layers_0 = LstmLayer(embedding_size, lstm_hidden_size, num_layers=lstm_num_layers)
+        self.lstm_layers_1 = LstmLayer(2*self.num_att*lstm_hidden_size, lstm_hidden_size, num_layers=lstm_num_layers)
+
+        self.att_layers_0 = MultiAttentionLayer(lstm_hidden_size*2, attention_size=attention_size, num_att=self.num_att)
+        self.att_layers_1 = MultiAttentionLayer(lstm_hidden_size*2, attention_size=attention_size, num_att=self.num_att)
+
+        self.final_linear = nn.Linear(2*self.num_att*lstm_hidden_size, output_size)
+
+    def forward(self, document, document_lengths, sequence_lengths):
+        """
+
+        :param document: [batch_size, max_sent, max_word]
+        :param document_lengths: [batch_size]
+        :param sequence_lengths: [batch_size, max_seq]
+        :return:
+        """
+        origin_shape = document.shape
+        # print("document_shape", document.shape)
+        flat_document = document.view(-1, origin_shape[-1])
+        # print("flat_document.shape", flat_document.shape)
+
+        # print("sequence_lengths", sequence_lengths.shape)
+        flat_sequence_lengths = sequence_lengths.view(-1)
+        # print("flat_sequence_lengths", flat_sequence_lengths.shape)
+
+        flat_document, flat_sequence_lengths = self.filter_sents(flat_document, flat_sequence_lengths)
+
+        flat_emb_document = self.word_embeddings(flat_document)
+        # flat_emb_document = emb_document.view(-1, emb_document.shape[-2], emb_document.shape[-1])
+        # print("flat_emb_document.shape", flat_emb_document.shape)
+
+        tokens_lstm = self.lstm_layers_0(flat_emb_document, flat_sequence_lengths)
+        tokens_att, _ = self.att_layers_0(tokens_lstm, flat_sequence_lengths)
+        # print("tokens_att.shape", tokens_att.shape)
+        tokens_att = self.make_tokens_att_full(tokens_att, document_lengths, origin_shape[0], origin_shape[1])
+        # print("full_tokens_att.shape", tokens_att.shape)
+
+        # tokens_att = tokens_att.view(emb_document.shape[0], emb_document.shape[1], -1)
+        assert tokens_att.shape[-1] == 256*2*self.num_att
+
+        sents_lstm = self.lstm_layers_1(tokens_att, document_lengths)
+        sents_att, _ = self.att_layers_1(sents_lstm, document_lengths)
+        # print("sents_att.shape", sents_att.shape)
+
+        final_outputs = self.final_linear(sents_att)
+        return final_outputs
--- hierarchical_attention_networks/train.py
+++ hierarchical_attention_networks/train.py
@@ -1,12 +1,10 @@
 import torch
 from .load_data import load_data
-from .model import HierarchicalAttention
 import torch.nn.functional as F
 import os
 import datetime
 import numpy as np
 import random
-
 
 SEED = 0
 
@@ -18,7 +16,6 @@ torch.backends.cudnn.benchmark = False
 
 CURRENT_DIR = os.path.dirname(__file__)
 BASENAME_DIR = os.path.basename(CURRENT_DIR)
-SAVE_DIR = f"{CURRENT_DIR}/models/{BASENAME_DIR}_{datetime.datetime.now().strftime('%y%m%d%-H%M%S'):}"
 
 
 def clip_gradient(model, clip_value):
@@ -64,9 +61,9 @@ def train_model(model, train_iter, epoch):
         count_true += num_corrects
         count_all += len(batch)
 
-        if (idx+1) % (len(train_iter) // 5) == 0:
+        if (idx + 1) % (len(train_iter) // 5) == 0:
             print(f'Epoch: {epoch + 1}, Idx: {idx + 1}, Training Loss: {loss.item():.4f}, '
-                  f'Training Accuracy: {count_true/count_all: .4f}%')
+                  f'Training Accuracy: {count_true / count_all: .4f}%')
 
     return total_epoch_loss / len(train_iter), count_true / count_all
 
@@ -106,22 +103,27 @@ def eval_model(model, data_iter):
 
 def main(args):
 
-    # prepare
+    print("args", vars(args))
+
+    # prepare save_dir
     assert not os.path.exists(SAVE_DIR)
+    print("SAVE_DIR", SAVE_DIR)
     os.makedirs(SAVE_DIR)
 
     # parameters:
     # learning_rate = 2e-5
     batch_size = args.batchsize
     # embedding_length = 200
-    embedding_length = 300
+    # embedding_length = 300
+    embedding_length = args.emb_size
     output_size = 2
     hidden_size = 256
 
     # prepare data:
-    TEXT, LABEL, vocab_size, word_embeddings, train_iter, valid_iter, test_iter = load_data(train_bsize=batch_size,
-                                                                                     bsize=batch_size * 2,
-                                                                                     embedding_length=embedding_length)
+    TEXT, LABEL, vocab_size, word_embeddings, \
+    train_iter, valid_iter, test_iter = load_data(train_bsize=batch_size,
+                                                  bsize=batch_size * 2,
+                                                  embedding_length=embedding_length)
     # torch.save(TEXT.vocab.stoi, 'models/TEXT.stoi.pt')
     # torch.save(LABEL.vocab.stoi, 'models/LABEL.stoi.pt')
     print('LABEL.vocab.stoi', LABEL.vocab.stoi)
@@ -129,10 +131,20 @@ def main(args):
     # LOAD MODEL
     torch.device('cuda:0')
 
-    model = HierarchicalAttention(output_size=output_size,
-                                  embedding_size=embedding_length,
-                                  embedding_weight=word_embeddings,
-                                  lstm_hidden_size=hidden_size)
+    if args.model == 'hierarchical':
+        from .model import HierarchicalAttention
+        model = HierarchicalAttention(output_size=output_size,
+                                      embedding_size=embedding_length,
+                                      embedding_weight=word_embeddings,
+                                      lstm_hidden_size=hidden_size)
+    elif args.model == 'multi_att':
+        from .model import HierarchicalMultiAttention
+        model = HierarchicalMultiAttention(output_size=output_size,
+                                           embedding_size=embedding_length,
+                                           embedding_weight=word_embeddings,
+                                           lstm_hidden_size=hidden_size)
+    else:
+        raise ValueError('Model kind = {}'.format(args.model))
 
     if torch.cuda.is_available():
         model.cuda()
@@ -148,7 +160,7 @@ def main(args):
         val_loss, val_acc, _, _ = eval_model(model, valid_iter)
 
         print("Epoch:{:4d}, loss:{}, acc:{}, "
-              "val_loss:{}, val_acc {}".format(epoch+1, train_loss, train_acc, val_loss, val_acc))
+              "val_loss:{}, val_acc {}".format(epoch + 1, train_loss, train_acc, val_loss, val_acc))
 
         save_path = f'{SAVE_DIR}/epoch:{epoch + 1:04d}_acc{val_acc:4f}_loss{val_loss:4f}.pth'
 
@@ -164,9 +176,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--batchsize", type=int, default=256)
     parser.add_argument("--epoch", type=int, default=25)
+    parser.add_argument("--emb_size", type=int, default=200, help='Embedding size')
     parser.add_argument('--pretrained', type=str, default=None)
+    parser.add_argument('--model', type=str, choices=['hierarchical', 'multi_att'], default='hierarchical')
     args = parser.parse_args()
+
+    SAVE_DIR = f"{CURRENT_DIR}/models/{args.model}_{datetime.datetime.now().strftime('%y%m%d%-H%M%S'):}"
 
     save_dir = main(args)
     from .run_test import run_test
-    run_test(save_dir)
+
+    run_test(save_dir, args)
